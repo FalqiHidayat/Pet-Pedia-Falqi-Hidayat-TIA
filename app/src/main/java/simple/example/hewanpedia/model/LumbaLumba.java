b695233e936472e796106311ff6e137aa6f26dc4
FalqiHidayat <falqijackson@gmail.com>
2020-11-22 19:55:37 +0700

    Falqi Hidayat TI/A NIM : 190602005

---
+++ app/src/main/java/simple/example/hewanpedia/model/LumbaLumba.java
@@ -0,0 +1,11 @@
+package simple.example.hewanpedia.model;
+
+
+public class LumbaLumba extends Hewan {
+
+
+    public LumbaLumba(String ras, String asal, String deskripsi, int drawableRes) {
+        super("LumbaLumba",ras,asal,deskripsi,drawableRes);
+    }
+
+}
